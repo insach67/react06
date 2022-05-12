808cd49da5e00268a79eb5082ee508c14758475d
insach67 <insach67@gmail.com>
2022-05-12 14:25:12 +0530

    third commit

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -1,34 +1,62 @@
-import React, {useState} from 'react'
+// import React, {useState} from 'react'
 
-export default function Classes() {
+// export default function Classes() {
 
-const [show, setShow] = useState(true)
-
-
-function tochangeclick(){
-  setShow(!show);
-}
+// const [show, setShow] = useState(true)
 
 
-  return (
+// function tochangeclick(){
+//   setShow(!show);
+// }
 
-    <div >
+
+//   return (
+
+//     <div >
      
-      <button className='text3' onClick={ tochangeclick}>To see styling in class component</button>
+//       <button className='text3' onClick={ tochangeclick}>To see styling in class component</button>
+//    {
+  
+//   show  ?  <div className='box2'> <h1 >This is Done using class component</h1>
+//      <p style={{ fontSize:"20px"}}>This is done using external css</p> 
+//      <p style={{color: "blue", fontSize:"20px"}}>This is done using inline css</p> </div> 
+//     : null
+  
+// }
+
+      
+      
+ 
+
+//     </div>
+//   )
+// }
+
+import React, { Component } from 'react'
+
+export default class Classes extends Component {
+  state={
+    showclass:true
+  }
+  changeclass=()=>{
+    this.setState({showclass:!this.state.showclass})
+  }
+  render() {
+    return (
+      <div >
+     
+      <button className='text3' onClick={ this.changeclass}>To see styling in class component</button>
    {
   
-  show  ?  <div className='box2'> <h1 >This is Done using class component</h1>
+  this.state.showclass ?  <div className='box2'> <h1 >This is Done using class component</h1>
      <p style={{ fontSize:"20px"}}>This is done using external css</p> 
      <p style={{color: "blue", fontSize:"20px"}}>This is done using inline css</p> </div> 
     : null
   
 }
 
-      
-      
- 
-
     </div>
-  )
+    )
+  }
 }
 
